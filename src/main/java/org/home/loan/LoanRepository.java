25c029c47ec59ebfe67676cdf13a94f5bfc91630
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2017-04-14 20:31:31 +0200

    Add ambiguous method

--- src/main/java/org/home/loan/LoanRepository.java
+++ src/main/java/org/home/loan/LoanRepository.java
@@ -1,10 +1,13 @@
 package org.home.loan;
 
 import org.home.loan.domain.Loan;
+import org.home.loan.domain.LoanApplication;
 
 public interface LoanRepository {
 
     Loan save(Loan loan);
+
+    Loan save(LoanApplication application);
 
     Loan findOne(Long id);
 
